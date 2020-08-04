0a54246f7e71d348a36fa9da2cee13409cf16b7f
Luca Hanel <lhanel@protonmail.com>
2020-08-04 23:04:18 +0200

    simplified testing

--- tests/rtl/common/testbench.h
+++ tests/rtl/common/testbench.h
@@ -6,8 +6,8 @@ vluint64_t main_time = 0;       // Current simulation time
 // used in Verilog (or from --timescale-override)
 
 double sc_time_stamp () {       // Called by $time in Verilog
-		return main_time;           // converts to double, to match
-																// what SystemC does
+	return main_time;           // converts to double, to match
+								// what SystemC does
 }
 
 template<class MODULE>	class TESTBENCH {
@@ -89,4 +89,168 @@ template<class MODULE>	class TESTBENCH {
 		}
 
 		virtual bool	done(void) { return (Verilated::gotFinish()); }
+
+		// -----------------------------------------------------------
+		// Debug interfacing functions
+		// -----------------------------------------------------------
+
+		// Read from memory address (can also be a memory mapped
+		// slave of the wishbone bus)
+		int read_mem(int addr){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_addr_i = addr;
+			this->m_core->dbg_cmd_i = 0x01;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			return (int) this->m_core->dbg_data_o;
+		}
+
+		// Write to memory address (can also be a memory mapped
+		// slave of the wishbone bus)
+		void write_mem(int addr, int data){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_addr_i = addr;
+			this->m_core->dbg_data_i = data;
+			this->m_core->dbg_cmd_i = 0x02;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			this->tick();
+		}
+
+		// Read from core registers
+		int read_reg(int reg){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_addr_i = reg;
+			this->m_core->dbg_cmd_i = 0x13;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			return (int) this->m_core->dbg_data_o;
+		}
+
+		// Write to core registers	
+		void write_reg(int reg, int data){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_addr_i = reg;
+			this->m_core->dbg_cmd_i = 0x14;
+			this->m_core->dbg_data_i = data;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			this->tick();
+		}
+
+		// Halt the core
+		void halt_core(){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x11;
+			this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+		}
+
+		// Resume the core
+		void resume_core(){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x12;
+			this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+		}
+
+		// Reset the core
+		void reset_core(){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x03;
+			this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+		}
+
+		// Read the program counter
+		int read_pc(){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x15;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			return (int) this->m_core->dbg_data_o;
+		}
+
+		// Set the program counter
+		void set_pc(int pc){
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x16;
+			this->m_core->dbg_data_i = pc;
+			this->tick();
+			while(this->m_core->dbg_ready_o == 0)
+				this->tick();
+			this->m_core->dbg_cmd_i = 0x0;
+			this->tick();
+		}
+
+
+		void load_program(int program[4096], int len, int startAddr){
+			this->halt_core();
+			for(int i = 0; i < len; i++){
+				this->write_mem(startAddr + 4*i, program[i]);
+			}
+		}
+
+		void load_program(char* filename, int startAddr){
+			int program[4096];
+			int instr_cnt = 0;
+			size_t len = 0;
+			ssize_t read;
+			char * line = NULL;
+    		FILE *fp;
+
+			// Read in the program
+			fp = fopen(filename, "r");
+			if(fp){
+				while((read = getline(&line, &len, fp)) != -1){
+					program[instr_cnt] = (int)strtol(line, NULL, 16);;
+					instr_cnt +=1;
+				}
+			} else {
+				std::cout << "Error reading dump" << std::endl;
+				exit(-10);
+			}
+
+			// Rest the core
+			this->reset();
+			// Halt the core
+			this->halt_core();
+			
+			// Load the program into memory
+    		this->load_program(program, instr_cnt, startAddr);
+
+			// Set core to start of program
+    		this->set_pc(0x0);
+
+			// Start the program
+			this->resume_core();
+		}
+
+
+		int check_memory(int *exp_mem, int n_checks){
+			for(int i = 0; i < 10; i++){
+				if(this->read_mem(exp_mem[i*2]) != exp_mem[i*2+1])
+					return i+1;
+			}
+			return 0;
+		}
 };
--- tests/rtl/tests/random_program_0/sim_main.cpp
+++ tests/rtl/tests/random_program_0/sim_main.cpp
@@ -2,202 +2,37 @@
 #include "Vcore_wrapper.h"
 #include "testbench.h"
 
-TESTBENCH<Vcore_wrapper> *tb;
-
-int read_mem(int addr){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_addr_i = addr;
-    tb->m_core->dbg_cmd_i = 0x01;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    return (int) tb->m_core->dbg_data_o;
-}
-
-int read_reg(int reg){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_addr_i = reg;
-    tb->m_core->dbg_cmd_i = 0x13;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    return (int) tb->m_core->dbg_data_o;
-}
-
-void write_reg(int reg, int data){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_addr_i = reg;
-    tb->m_core->dbg_cmd_i = 0x14;
-    tb->m_core->dbg_data_i = data;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    tb->tick();
-}
-
-void write(int addr, int data){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_addr_i = addr;
-    tb->m_core->dbg_data_i = data;
-    tb->m_core->dbg_cmd_i = 0x02;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    tb->tick();
-}
-
-void halt_core(){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x11;
-    tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-}
-
-void resume_core(){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x12;
-    tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-}
-
-void reset_core(){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x03;
-    tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-}
-
-int read_pc(){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x15;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    return (int) tb->m_core->dbg_data_o;
-}
-
-void set_pc(int pc){
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x16;
-    tb->m_core->dbg_data_i = pc;
-    tb->tick();
-    while(tb->m_core->dbg_ready_o == 0)
-        tb->tick();
-    tb->m_core->dbg_cmd_i = 0x0;
-    tb->tick();
-}
-
-void load_program(int program[1024], int len, int startAddr){
-    halt_core();
-    for(int i = 0; i < len; i++){
-        write(startAddr + 4*i, program[i]);
-    }
-}
-
-int test_registers(){
-    // Register 0 should never be changed
-    write_reg(0x0, 0xff);
-    if(read_reg(0x0) != 0x0)
-        return 1;
-    
-    for(int i = 1; i < 32; i++){
-        write_reg(i, 0xf00+i);
-        if(read_reg(i) != (0xf00+i))
-            return i;
-        // Clear register again
-        write_reg(i, 0x0);
-    }
-
-    return 0;
-}
 
 int main(int argc, char** argv, char** env) {
-    int program[1024];
-    int instr_cnt = 0;
-    size_t len = 0;
-    ssize_t read;
-    char * line = NULL;
-    int exp_mem[10][2];
-    FILE *fp;
-    fp = fopen("main.hex", "r");
-    if(fp){
-        while((read = getline(&line, &len, fp)) != -1){
-            program[instr_cnt] = (int)strtol(line, NULL, 16);;
-            instr_cnt +=1;
-        }
-    } else {
-        std::cout << "Error reading dump" << std::endl;
-        exit(-10);
-    }
-
-    // fclose(fp);
-    // if(line != NULL){free(line);}
-    for(int i = 0; i < 10; i++){
-        exp_mem[i][0] = 100+i*4;
-        exp_mem[i][1] = i;
-    }
-    
+    TESTBENCH<Vcore_wrapper> *tb;
     Verilated::commandArgs(argc, argv);
     tb = new TESTBENCH<Vcore_wrapper>();
     tb->opentrace("logs/trace.vcd");
 
     int result = 0;
-
-    // Initialize inputs
-
-    // Reset
-    tb->reset();
-
-    // Stop the core
-    halt_core();
-
-    // Test reading and writing to register        
-    result = test_registers();
-    if(result != 0){
-        std::cout << "regcheck FAILED " << result << std::endl;
-        exit(0);
+    int exp_mem[10][2];    
+    for(int i = 0; i < 10; i++){
+        exp_mem[i][0] = 100+i*4;
+        exp_mem[i][1] = i;
     }
-    // Load the program
-    load_program(program, instr_cnt, 0x0);
-    // Resume the core
-    set_pc(0x0);
-    resume_core();
+
+    tb->load_program("main.hex", 0x0);
 
     for(int i = 0; i < 1000; i++){
         tb->tick();
     }
-    int data;
-    for(int i = 0; i < 10; i++){
-        if(data = read_mem(exp_mem[i][0]) != exp_mem[i][1])
-            std::cout << "FAILED " << data << std::endl;
-        else
-            std::cout << "PASSED" << std::endl;
-    }
-    
+
+    result = tb->check_memory((int*) exp_mem, 10);
 
     // Cleanup
     tb->tick();
     tb->m_core->final();
 
     // Evaluate test
-    // if(result == 0)
-    //     std::cout << "PASSED" << std::endl;
-    // else
-    //     std::cout << "FAILED " << result << std::endl;
+    if(result == 0)
+        std::cout << "PASSED" << std::endl;
+    else
+        std::cout << "FAILED " << result << std::endl;
     
     //  Coverage analysis (since test passed)
     VerilatedCov::write("logs/coverage.dat");
